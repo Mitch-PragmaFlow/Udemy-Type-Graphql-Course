c1f9a2d71aee3d6fb7c6b8d7c833842bb2f8d7dd
Mitch <72631198+Mitch-PragmaFlow@users.noreply.github.com>
2022-08-09 16:18:18 -0400

    fix

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,7 +4,9 @@ import path from "path";
 
 export const getSchema = () => {
     const schema = makeSchema({
-        types: [Query],
+        types: [
+            Query
+        ],
         outputs: {
             schema: path.join(process.cwd(), 'nexus', 'schema.graphql'),
             typegen: path.join(process.cwd(), 'nexus', 'nexus.ts'),
